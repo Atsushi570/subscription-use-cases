489dba0737819377d4322596490a9439ab99d917
Atsushi Sato <sato.atsushi570@gmail.com>
2022-03-04 00:16:10 +0900

    顧客情報のメタデータを追加

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,39 +1,46 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import "./App.css";
+import { Redirect } from "react-router-dom";
 
 const Register = (props) => {
-  const [email, setEmail] = useState('jenny.rosen@example.com');
+  const [email, setEmail] = useState("jenny.rosen@example.com");
   const [customer, setCustomer] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const {customer} = await fetch('/create-customer', {
-      method: 'post',
+    const { customer } = await fetch("/create-customer", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
+        name: "test name",
+        metadata: {
+          test: "test property",
+        },
       }),
-    }).then(r => r.json());
+    }).then((r) => r.json());
 
     setCustomer(customer);
   };
 
-  if(customer) {
-    return <Redirect to={{pathname: '/prices'}} />
+  if (customer) {
+    return <Redirect to={{ pathname: "/prices" }} />;
   }
 
   return (
     <main>
       <h1>Sample Photo Service</h1>
 
-      <img src="https://picsum.photos/280/320?random=4" alt="picsum generated" width="140" height="160" />
+      <img
+        src="https://picsum.photos/280/320?random=4"
+        alt="picsum generated"
+        width="140"
+        height="160"
+      />
 
-      <p>
-        Unlimited photo hosting, and more. Cancel anytime.
-      </p>
+      <p>Unlimited photo hosting, and more. Cancel anytime.</p>
 
       <form onSubmit={handleSubmit}>
         <label>
@@ -43,15 +50,14 @@ const Register = (props) => {
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            required />
+            required
+          />
         </label>
 
-        <button type="submit">
-          Register
-        </button>
+        <button type="submit">Register</button>
       </form>
     </main>
   );
-}
+};
 
 export default Register;
